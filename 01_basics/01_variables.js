e7c3d48f221b5dc5771de745a56265439641b5c3
PrathameshLohar32 <97296552+PrathameshLohar32@users.noreply.github.com>
2023-09-26 19:01:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12344 // const cannot be changed
+let accountEmail= "pkl@gmail.com"
+var accountPass = "12345"   // try not to use var because of issue in block scope
+accountCity="Pune"
+
+
+//accountId=2    // Not allowed to change constant
+console.table([accountId, accountEmail,accountPass,accountCity])

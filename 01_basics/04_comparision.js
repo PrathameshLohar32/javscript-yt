96d8dc3aba15a54608ce551b5eed6b4a8b041387
PrathameshLohar32 <97296552+PrathameshLohar32@users.noreply.github.com>
2023-09-27 18:41:17 +0000

    comparision operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+// data types of operands must be same while using comparing operator
+// basic operators are same >,<,==,etc
+
+//strict check operator ===     -> this checks datatype as well as value
+//Eg,
+
+console.log("2"==2) //It will give true
+console.log("2"===2) // It will give false
+

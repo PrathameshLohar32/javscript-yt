e08e07215d4294469f3dc2bc64895d1915a05d5d
PrathameshLohar32 <97296552+PrathameshLohar32@users.noreply.github.com>
2023-09-27 17:58:49 +0000

    conversion of datatype

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,15 @@
+let score="33"
+console.log(Number(score)) //pure number represented as string can be converted to number.
+
+let sc="33aa" // this is also converted but shows NaN after print
+
+let isloggedIN = 1;
+let boolconversion = Boolean(isloggedIN)
+console.log(isloggedIN)
+// "33" => 33
+// "33aa" => NaN
+// true => 1, false=>0
+// "" => false,  "ahxva" => true
+let num= "34365";
+let convtoString = String(num);
+console.log(convtoString)

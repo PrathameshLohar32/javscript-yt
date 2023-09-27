5777235c132eeb950bd9e6206aebd38f2c14c0bb
PrathameshLohar32 <97296552+PrathameshLohar32@users.noreply.github.com>
2023-09-27 17:18:19 +0000

    01_basics/02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let age=18
+let name="Prathamesh"
+let isLoggedIn=false
+
+//number  2^53
+//bigint for larger number than above
+//string => ""
+//boolean => true, false
+//null => standalone value
+//symbol
+
+console.log(typeof "hello")
+console.log(typeof age)
